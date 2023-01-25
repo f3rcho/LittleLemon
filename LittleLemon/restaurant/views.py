1de64b400165882a754a49944a57d4eb3121edf3
Fernando Cordero <fernan942@gmail.com>
2023-01-25 09:54:36 -0300

    ADD: viewsetmodel
    router

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
